c24689253bedb17b9f32844988140f11a7241849
Daineair <sces2859615s@gmail.com>
2023-05-17 14:40:30 +0800

    w14 0517

---
+++ src/App_28.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [products, setProducts ] = useState([]);
+
+  const getShop2_28 = async () => {
+    try{
+      const response = await fetch(`https://jiewhttktusvivcyqnki.supabase.co/rest/v1/shop2_28?select=*`, {
+        method: 'GET',
+        headers: {
+          apikey: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppZXdodHRrdHVzdml2Y3lxbmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0ODc5MTAsImV4cCI6MTk5ODA2MzkxMH0.Hu3m_m1VKVZ22aFHQt6sB-t5fgHCzlnCOvjC_DFaTzU`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppZXdodHRrdHVzdml2Y3lxbmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0ODc5MTAsImV4cCI6MTk5ODA2MzkxMH0.Hu3m_m1VKVZ22aFHQt6sB-t5fgHCzlnCOvjC_DFaTzU`
+        }
+      });
+      const data = await response.json();
+      console.log('shop2 data', data);
+      setProducts(data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getShop2_28();
+  }, []);
+
+  return (
+    <div className="shop-page">
+      <div className="collection-page">
+      <h1 className="title">All Products -- Supabase shop2</h1>
+      <div className="items">
+        {products.map( (product) => {
+          const {name, price , local_url} = product;
+          return (
+            <div className="collection-item">
+            <img className="image" src={local_url} />
+            <div className="collection-footer">
+              <span className="name">{name}</span>
+              <span className="price">{price}</span>
+            </div>
+            <button className="custom-button">Add to Cart</button>
+          </div>
+          )
+        })}
+         
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/SneakersProductStaticPage_28.js
@@ -0,0 +1,88 @@
+import logo from './logo.svg';
+import './App.css';
+
+function SneakersProductStaticPage_28() {
+  return (
+    <div className="shop-page">
+      <div className="collection-page">
+      <h1 className="title">Sneakers</h1>
+      <div className="items">
+         <div className="collection-item">
+           <img className="image" src="https://i.ibb.co/0s3pdnc/adidas-nmd.png" />
+           <div className="collection-footer">
+             <span className="name">Adidas NMD</span>
+             <span className="price">220</span>
+           </div>
+           <button className="custom-button">Add to Cart</button>
+         </div>
+         <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/dJbG1cT/yeezy.png" />
+          <div className="collection-footer">
+            <span className="name">Adidas Yeezy</span>
+            <span className="price">280</span>
+          </div>
+          <button className="custom-button">Add to Card</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/bPmVXyP/black-converse.png" />
+          <div className="collection-footer">
+            <span className="name">Black Converse</span>
+            <span className="price">110</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/1RcFPk0/white-nike-high-tops.png" />
+          <div className="collection-footer">
+            <span className="name">Nike White AirForce</span>
+            <span className="price">160</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/QcvzydB/nikes-red.png" />
+          <div className="collection-footer">
+            <span className="name">Nike Brown High Tops</span>
+            <span className="price">160</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/w4k6Ws9/nike-funky.png" />
+          <div className="collection-footer">
+            <span className="name">Air Jordan Limited</span>
+            <span className="price">190</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/Mhh6wBg/timberlands.png" />
+          <div className="collection-footer">
+            <span className="name">Timberlands</span>
+            <span className="price">200</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/1f2nWMM/wolf-cap.png" />
+          <div className="collection-footer">
+            <span className="name">Wolf Cap</span>
+            <span className="price">14</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/X2VJP2W/blue-snapback.png" />
+          <div className="collection-footer">
+            <span className="name">Blue Snapback</span>
+            <span className="price">16</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
